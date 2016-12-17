28acd38dc49ae9790764d2c5feb86e9d6baf280d
zhenxhuo <zhen@Nott>
2016-12-18 00:49:22 +0800

    update fxml

---
+++ DiamondHunter/src/application/Controller.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+public class Controller {
+	
+	Button button;
+	
+	@FXML
+	public void LoadMap(){
+		button.getOnAction();
+	}
+
+	
+	
+}
