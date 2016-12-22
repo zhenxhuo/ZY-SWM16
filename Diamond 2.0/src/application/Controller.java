2e993a614f656552bd79a7fdf6ea00e43ae01bbb
zhenxhuo <khcy5hzx@nottingham.edu.my>
2016-12-23 01:03:05 +0800

    Latest

---
+++ Diamond 2.0/src/application/Controller.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import com.neet.DiamondHunter.TileMap.*;
+
+@SuppressWarnings("unused")
+public class Controller {
+	
+	Button button;
+	
+	@FXML
+	public void LoadMap(){
+		button.setOnAction(e->{
+			
+		});
+			
+		
+	}
+
+	
+	
+}
